8499475a25530c3f13992769fb4c2affb8a20400
alex-alexandrade <alexandrade1516@hotmail.com>
2022-04-25 09:20:48 -0300

    first commit

---
+++ eventos.js
@@ -0,0 +1,19 @@
+function clickMe(){
+    prompt("Fui clicado!")
+}
+
+function focusMe(){
+    console.log("Fui focado!")
+}
+
+function blurMe(){
+    console.log("Fui desfocado!")
+}
+
+function changerMe(){
+    console.log("Fui alterado!")
+}
+
+function overMe(){
+    console.log("Mouse aqui!")
+}
